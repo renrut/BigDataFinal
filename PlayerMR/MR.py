39c25e51a5c78dae21fad0a7b5cf78277b2b8762
csx283vu <turnerstrayhorn@Turners-MacBook-Pro.local>
2016-04-27 01:10:32 -0500

    MRJobs

--- PlayerMR/MR.py
+++ PlayerMR/MR.py
@@ -1,16 +1,11 @@
 
 from mrjob.job import MRJob
-from mr3px.csvprotocol import CsvProtocol
 import sys
 import os
-import csv
+import champions
 
-'''
-Count the number of tweets. 
-Parse tweets with json.loads -- note how the tweets are huge JSON blobs.
-Ignore tweets that error on load.
-'''
 
+#gets average player weight for each team
 class PlayerWeight(MRJob):
     # OUTPUT_PROTOCOL = CsvProtocol
     # INPUT_PROTOCOL = CsvProtocol
@@ -32,20 +27,81 @@ class PlayerWeight(MRJob):
 
 
 
-
+#counts winning colleges
 class Colleges(MRJob):
 
     def mapper(self, _, line):
         try:
-            #line.split(',')
-            yield "player", 1
+            data = line.split(',')
+            college = data[7]
+            team = data[8]
+            year = int(data[9])
+            win = champions.isChamp(team, year)
+            yield college, win
         except:
             pass
         
-    def reducer(self, _, counts):
-        yield 'players', sum(counts)
+    def reducer(self, college, win):
+        wincount = sum(win)
+        if wincount>0:
+            yield college, wincount
+
+#0 31
+#1 Mike Muscala
+#2 PF
+#3 11-Jun
+#4 240
+#5 July 1 1991
+#6 R
+#7 Bucknell University
+#8 ATL
+#9 2014
+#POS, WEIGHT, AGE, WIN
+class TeamAvg(MRJob):
+    print "[win,pos] [avg. age, avg. weight]"
+    def mapper(self, _, line):
+        data = line.split(',')
+        pos = data[2]
+        year = int(data[9])
+        age = year - int(data[5][-4:])
+        weight = int(data[4])
+        team = data[8]
+        win = champions.isChamp(team, year)
+        yield (win,pos), (age, weight)
 
 
+    def reducer(self, win, player):
+        player = list(player)
+        tot = sum([p[0] for p in player])
+        ageavg = tot/len(player)
+        weightavg = sum([p[1] for p in player])/len(player)
+        yield win, (ageavg, weightavg)
+
+#MR Job gets the average number of player seasons for team
+class SeasonCount(MRJob):
+    print "[win,pos] [avg. age, avg. weight]"
+    def mapper(self, _, line):
+        data = line.split(',')
+        year = int(data[9])
+        seasons = data[6]
+        if seasons is "R":
+            seasons = 0
+        seasons = int(seasons)
+        team = data[8]
+        win = champions.isChamp(team, year)
+        yield (win, team, year), seasons
+
+
+    def reducer(self, win, seasons):
+        seasons = list(seasons)
+        seasonsavg = sum(seasons)/len(seasons)
+        yield win, seasonsavg
+
+
+#output should be avg weight for PG, C, PF, SF, SG on winning teams
     
 if __name__ == '__main__':
-    PlayerWeight.run()
+    SeasonCount.run()
+
+
+
