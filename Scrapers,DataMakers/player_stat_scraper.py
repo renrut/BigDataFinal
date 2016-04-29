2e9f0cb593f30b9b3c2909b2821b81d701572fb3
Henry Bogardus <henry.bogardus@gmail.com>
2016-04-29 13:00:27 -0500

    fadf

--- Scrapers,DataMakers/player_stat_scraper.py
+++ Scrapers,DataMakers/player_stat_scraper.py
@@ -5,9 +5,10 @@ import pandas as pd
 import requests
 
 reg = []
-play = []
+df = pd.read_csv('data/season_stats_regular_season.csv')
 
-for year in range(1996,2016):
+for index, row in df.iterrows():
+	year = row.SEASON_ID - 40000
 	second_half = ''
 	if year<1999:
 		second_half = str(year+1-1900)
@@ -17,36 +18,48 @@ for year in range(1996,2016):
 		second_half = str(year+1-2000)
 
 	season = str(year)+"-"+second_half
-	TEAM_ID = 1000000001
-	requestReg = "http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season="+season+"&SeasonSegment=&SeasonType=Regular+Season&TeamID="+TEAM_ID+"7&VsConference=&VsDivision="
-	requestPlayoff = "http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season="+season+"&SeasonSegment=&SeasonType=Playoffs&TeamID="+TEAM_ID+"&VsConference=&VsDivision="
+	TeamID = row.TEAM_ID
+	print str(row.TEAM_ID)+", "+season
+	requestReg = "http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season="+season+"&SeasonSegment=&SeasonType=Regular+Season&TeamID="+str(TeamID)+"&VsConference=&VsDivision="
 	
 	url = "/usr/bin/open -a '/Applications/Google Chrome.app' '" + requestReg + "'"
 	os.system(url)
-	url = "/usr/bin/open -a '/Applications/Google Chrome.app' '" + requestPlayoff + "'"
-	os.system(url)
 
-	time.sleep(5)
+	time.sleep(3)
 	
 	responseReg = requests.get(requestReg, timeout=20)
-	dataReg = responseReg.json()['resultSets'][1]['rowSet']
-	headersReg = responseReg.json()['resultSets'][1]['headers']
-
-	responsePlay = requests.get(requestPlayoff, timeout=20)
-	dataPlay = responsePlay.json()['resultSets'][1]['rowSet']
-	headersPlay = responsePlay.json()['resultSets'][1]['headers']
-
+	try:
+		dataReg = responseReg.json()['resultSets'][1]['rowSet']
+		headersReg = responseReg.json()['resultSets'][1]['headers']
+	except:
+		try:
+			dataReg = responseReg.json()['resultSets'][1]['rowSet']
+			headersReg = responseReg.json()['resultSets'][1]['headers']
+		except:
+			try:
+				dataReg = responseReg.json()['resultSets'][1]['rowSet']
+				headersReg = responseReg.json()['resultSets'][1]['headers']
+			except:
+				try:
+					dataReg = responseReg.json()['resultSets'][1]['rowSet']
+					headersReg = responseReg.json()['resultSets'][1]['headers']
+				except:
+					try:
+						dataReg = responseReg.json()['resultSets'][1]['rowSet']
+						headersReg = responseReg.json()['resultSets'][1]['headers']
+					except:
+						pass
+					pass
+				pass
+			pass
+		pass
 	regDF = pd.DataFrame(dataReg, columns=headersReg)
-	regDF.insert(0, "TEAM_ID",TEAM_ID)
+	regDF.insert(0, "TEAM_ID", TeamID)
 	regDF.insert(0, "SEASON_ID", year+40000)
-	playDF = pd.DataFrame(dataPlay, columns=headersPlay)
-	playDF.insert(0, "TEAM_ID",TEAM_ID)
-	playDF.insert(0, "SEASON_ID", year+40000)
 	reg.append(regDF)
-	play.append(playDF)
 
 	print str(year)+".....done"
 	# print output
 
-pd.concat(reg).to_csv('data/season_stats_regular_season.csv')
-pd.concat(play).to_csv('data/season_stats_playoffs.csv')
+pd.concat(reg).to_csv('data/player_stats_regular_season.csv',index=False)
+
--- Scrapers,DataMakers/season_stat_scraper.py
+++ Scrapers,DataMakers/season_stat_scraper.py
@@ -5,8 +5,6 @@ import pandas as pd
 import requests
 
 reg = []
-play = []
-
 for year in range(1996,2016):
 	second_half = ''
 	if year<1999:
@@ -18,35 +16,34 @@ for year in range(1996,2016):
 
 	season = str(year)+"-"+second_half
 	requestReg = "http://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season="+season+"&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision="
-	requestPlayoff = "http://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season="+season+"&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision="
 	
 	url = "/usr/bin/open -a '/Applications/Google Chrome.app' '" + requestReg + "'"
 	os.system(url)
-	url = "/usr/bin/open -a '/Applications/Google Chrome.app' '" + requestPlayoff + "'"
-	os.system(url)
 
-	time.sleep(5)
+	time.sleep(4)
 	
 	responseReg = requests.get(requestReg, timeout=20)
-	dataReg = responseReg.json()['resultSets'][0]['rowSet']
-	headersReg = responseReg.json()['resultSets'][0]['headers']
+	try:
+		dataReg = responseReg.json()['resultSets'][0]['rowSet']
+		headersReg = responseReg.json()['resultSets'][0]['headers']
+	except:
+		try:
+			dataReg = responseReg.json()['resultSets'][0]['rowSet']
+			headersReg = responseReg.json()['resultSets'][0]['headers']
+		except:
+			pass
+		pass
 
-	responsePlay = requests.get(requestPlayoff, timeout=20)
-	dataPlay = responsePlay.json()['resultSets'][0]['rowSet']
-	headersPlay = responsePlay.json()['resultSets'][0]['headers']
 
 	regDF = pd.DataFrame(dataReg, columns=headersReg)
 	regDF.insert(0, "SEASON_ID", year+40000)
-	playDF = pd.DataFrame(dataPlay, columns=headersPlay)
-	playDF.insert(0, "SEASON_ID", year+40000)
 	reg.append(regDF)
-	play.append(playDF)
 
 	print str(year)+".....done"
 	# print output
 
-pd.concat(reg).to_csv('data/season_stats_regular_season.csv')
-pd.concat(play).to_csv('data/season_stats_playoffs.csv')
+pd.concat(reg).to_csv('data/season_stats_regular_season.csv',index=False)
+
 
 
 
